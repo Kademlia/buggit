39606dd65643d485354945f8f1556cccb20230d3
Kademlia <kademlia@kadcon.de>
2023-11-07 19:38:45 +0100

    Lombok crash example

---
+++ src/main/java/org/bukkit/Rip.java
@@ -0,0 +1,18 @@
+package org.bukkit;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Rip {
+
+	
+	public static void materialTest(Material mat) {
+		if(mat == Material   // Uncomment this line and start typing '.coal',
+		
+		// This will result in memory rapidly increasing until the set maximum of eclipse
+		// After that the CPU will go to 100% and stay there
+		// With -Xmx8G the IDE survives extremely slow typing. 
+		// But will still crash when typing fast
+	}
+	
+}
